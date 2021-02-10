1473a1e74ea35dbc80aa6612550bdddb587ffd55
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-02-10 22:32:51 +0530

    Create a Queue Of 56->30->70

---
+++ MyQueue.java
@@ -0,0 +1,32 @@
+package bridgelabz;
+
+public class MyQueue {
+
+    public final MyLinkedList myLinkedList;
+
+    public MyQueue() {
+        myLinkedList = new MyLinkedList();
+    }
+
+    public void enqueue(MyNode element) {
+        myLinkedList.append(element);
+    }
+
+    public void printStack() {
+        myLinkedList.printMyNodes();
+    }
+
+    public boolean isEmpty() {
+        return myLinkedList.head == null;
+    }
+
+    public INode peak() {
+        if (!isEmpty()) {
+            return myLinkedList.head;
+        }
+        else {
+            System.out.println("Queue is empty!!");
+        }
+        return null;
+    }
+}
